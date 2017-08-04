7c47e8c3ceaffd09265cd78c6d787e32ab87f765
Otavio Augusto <tavioalves@gmail.com>
2017-08-04 17:19:36 -0300

    Criação de panel e funcionalidades

--- tictactoe.js
+++ tictactoe.js
@@ -7,7 +7,7 @@ var grid = [
   [0, 0, 0]
 ];
 
-$(".box").click(function(){  
+$(".box").click(function(){
   if ($(this).find('#x').length == 0 && $(this).find('#o').length == 0 ) {
     if(turnX){
       $(this).append('<p id="x" class="value">X</p>');
@@ -26,14 +26,14 @@ $(".box").click(function(){
 function makeMove(row, column) {
   if (grid[row][column] == 0) {
     grid[row][column] = turn;
- 
+
     if (turn == 1) {
       turn = -1;
     } else {
       turn = 1;
     }
     isGameOver();
-  } 
+  }
 }
 
 function isGameOver() {
@@ -43,21 +43,40 @@ function isGameOver() {
         return;
       }
   }
-  for (var i = 0; i < grid.length; i++) {      
+  for (var i = 0; i < grid.length; i++) {
       if(grid[0][i] == grid[1][i] && grid[0][i]==grid[2][i]  && grid[0][i]!=0){
         alert(grid[0][i]+" Wins");
         return;
       }
-  }  
+  }
   if(grid[0][0]==grid[1][1] && grid[0][0] == grid[2][2]  && grid[0][0]!=0){
     alert(grid[0][0]+" Wins");
     return;
   }
-  
+
   if(grid[0][2]==grid[1][1] && grid[0][2] == grid[2][0]  && grid[2][0]!=0){
     alert(grid[1][1]+" Wins");
     return;
-  }  
+  }
 }
 
+function showAll(){
+  if($("#board").css('display') == 'none')
+    $("#board").fadeIn( "slow" );
+  if($("#panel").css('display') == 'none')
+    $("#panel").fadeIn( "slow" );
+};
 
+function showBoard(){
+  if($("#board").css('display') == 'none')
+    $("#board").fadeIn( "slow" );
+  if($("#panel").css('display') == 'block')
+    $("#panel").fadeOut( "slow" );
+};
+
+function showPanel(){
+  if($("#panel").css('display') == 'none')
+    $("#panel").fadeIn( "slow" );
+  if($("#board").css('display') == 'block')
+    $("#board").fadeOut( "slow" );
+};
