c439175fa2f2b3e38edd1e47fadbfb08e1948e65
Otavio Augusto <tavioalves@gmail.com>
2017-08-14 16:14:12 -0300

    Funcionalidades de novo jogo aplicadas

--- tictactoe.js
+++ tictactoe.js
@@ -1,6 +1,7 @@
 var turnX = true;
 var turn = 1;
 var gameOption;
+var gameOver = false;
 
 var grid = [
   [0, 0, 0],
@@ -8,98 +9,86 @@ var grid = [
   [0, 0, 0]
 ];
 
-var delayMillis = 3000; //1 second
-
-setTimeout(function() {
-  //your code to be executed after 1 second
-
-  function showAll(){
-    if($("#board").css('display') == 'none')
-      $("#board").fadeIn( "slow" );
-    if($("#panel").css('display') == 'none')
-      $("#panel").fadeIn( "slow" );
-  };
-
-  function showBoard(){
-    if($("#board").css('display') == 'none')
-      $("#board").fadeIn( "slow" );
-    if($("#panel").css('display') == 'block')
-      $("#panel").fadeOut( "slow" );
-  };
-
-  function showPanel(){
-    if($("#panel").css('display') == 'none')
-      $("#panel").fadeIn( "slow" );
-    if($("#board").css('display') == 'block')
-      $("#board").fadeOut( "slow" );
-  };
-}, delayMillis);
+$(".box").click(function(){
+  if(gameOver == false) {
+    if ($(this).find('#x').length == 0 && $(this).find('#o').length == 0 ) {
+      if(turnX){
+        $(this).append('<p id="x" class="value">X</p>');
+        turnX = false;
+      } else {
+        $(this).append('<p id="o" class="value">O</p>');
+        turnX = true;
+      }
+    } else {
+      console.log("Occupied space!");
+    }
+  } else {
+    console.log("The game is over!");
+  }
+});
 
 $(document).ready(function(){
-    chooseGameOption();
-})
+    newGame();
+});
 
 function chooseGameOption() {
   $("#panel-message").append("How do you want to play?").hide().fadeIn(1500);
-  $("#panel-buttons").append('<button class="panel-button" type="button" name="button">One Player</button><button class="panel-button" type="button" name="button" onclick="twoPlayersOption()">Two Players</button>').hide().fadeIn(1500);
+  $("#panel-buttons").append('<button class="panel-button" type="button" name="button">One Player</button><button class="panel-button" type="button" name="button" onclick="twoPlayers()">Two Players</button>').hide().fadeIn(1500);
 }
 
-function onePlayerOption() {
-
-}
-
-function appendBoard() {
-  $("#tictactoe-game").empty();
-  $("#tictactoe-game").hide().append('<div id="board"><div class="internal-shadow"><div style="padding-top: 2.5px;"><div class="line"><div onclick="makeMove(0,0)" id="box1" value="1" class="box"></div><div onclick="makeMove(0,1)" id="box2" value="2" class="box"></div><div onclick="makeMove(0,2)" id="box3" value="3" class="box"></div></div><div class="line"><div onclick="makeMove(1,0)" id="box4" value="4" class="box"></div><div onclick="makeMove(1,1)" id="box5" value="5" class="box"></div><div onclick="makeMove(1,2)" id="box6" value="6" class="box"></div></div><div class="line"><div onclick="makeMove(2,0)" id="box7" value="7" class="box"></div><div onclick="makeMove(2,1)" id="box8" value="8" class="box"></div><div onclick="makeMove(2,2)" id="box9" value="9" class="box"></div></div></div></div></div>').fadeIn(1500);
-}
-
-function twoPlayersOption() {
+function twoPlayers() {
   if($("#board").css('display') == 'none')
     $("#board").fadeIn( "slow" );
   if($("#panel").css('display') == 'block')
     $("#panel").fadeOut( "slow" );
 }
 
-function resetGame() {
+function newGame() {
+  console.log("New game was selected!");
 
-}
+  gameOver = false;
 
-function showWinner(winner) {
-  $("#panel-message").empty();
-  $("#panel-buttons").empty();
+  clearPanel();
+  clearBoard();
 
-  if($("#panel").css('display') == 'none')
-    $("#panel").fadeIn( "slow" );
-
-  if(winner == -1) {
-    $("#panel-message").append("Player One Won!").hide().fadeIn(1500);
-    $("#panel-buttons").append('<button class="panel-button" type="button" name="button">Restart</button>').hide().fadeIn(1000);
-    $("#panel-buttons").append('<button class="panel-button" type="button" name="button">New Game</button>').hide().fadeIn(1000);
-  } else {
-    $("#panel-message").append("Player Two Won!").hide().fadeIn(1500);
-    $("#panel-buttons").append('<button class="panel-button" type="button" name="button">Restart</button>').hide().fadeIn(1000);
-    $("#panel-buttons").append('<button class="panel-button" type="button" name="button">New Game</button>').hide().fadeIn(1000);
-  }
-}
-
-
-function fadeTest() {
   chooseGameOption();
 }
 
-$(".box").click(function(){
-  if ($(this).find('#x').length == 0 && $(this).find('#o').length == 0 ) {
-    if(turnX){
-      $(this).append('<p id="x" class="value">X</p>');
-      turnX = false;
-    } else {
-      $(this).append('<p id="o" class="value">O</p>');
-      turnX = true;
-    }
-  } else {
-    console.log("Occupied space!");
+function clearPanel() {
+  $("#panel-message").empty();
+  $("#panel-buttons").empty();
+}
+
+function clearBoard() {
+  for(i = 0; i <= 9; i++){
+    $('#box'+i).empty();
   }
-});
+
+  grid = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+  ];
+
+  $("#board").hide().fadeOut(1500);
+}
+
+function showWinner(winner) {
+  clearPanel();
+  $("#panel").fadeIn("slow");
+
+  if(winner == 1) {
+    $("#panel-message").append("Player X Won!").hide().fadeIn(1500);
+    endGameOptions();
+  } else {
+    $("#panel-message").append("Player O Won!").hide().fadeIn(1500);
+    endGameOptions();
+  }
+}
+
+function endGameOptions(){
+  $("#panel-buttons").append('<button onClick="newGame();" class="panel-button" type="button" name="button">New Game</button>').hide().fadeIn(1000);
+}
 
 function makeMove(row, column) {
   if (grid[row][column] == 0) {
@@ -118,22 +107,26 @@ function isGameOver() {
   for (var i = 0; i < grid.length; i++) {
       if(grid[i][0] == grid[i][1] && grid[i][0]==grid[i][2] && grid[i][0]!=0){
         showWinner(grid[i][0]);
+        gameOver = true;
         return;
       }
   }
   for (var i = 0; i < grid.length; i++) {
       if(grid[0][i] == grid[1][i] && grid[0][i]==grid[2][i]  && grid[0][i]!=0){
         showWinner(grid[0][i]);
+        gameOver = true;
         return;
       }
   }
   if(grid[0][0]==grid[1][1] && grid[0][0] == grid[2][2]  && grid[0][0]!=0){
     showWinner(grid[0][0]);
+    gameOver = true;
     return;
   }
 
   if(grid[0][2]==grid[1][1] && grid[0][2] == grid[2][0]  && grid[2][0]!=0){
     showWinner(grid[1][1]);
+    gameOver = true;
     return;
   }
 }
